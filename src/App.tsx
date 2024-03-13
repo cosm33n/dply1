0534e13c67c6809a2a7620a679983ecf4b6deb64
Cosmin Moisa <cosm33n@gmail.com>
2024-03-13 11:02:45 +0200

    readd base in vite.config

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,12 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
-        <p>
-          am sters <strong>base u</strong>
+        <p
+          style={{
+            color: 'green',
+          }}
+        >
+          nu am mai sters <strong>base u</strong>
         </p>
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
